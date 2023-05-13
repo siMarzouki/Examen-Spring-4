8a7f339a0922630d79a8aea4b5d51ef618e5ece8
siMarzouki <marzouki.mohamedaziz@esprit.tn>
2023-05-13 22:13:37 +0100

    first commit

---
+++ corrigé/src/main/java/tn/esprit/Services/Services.java
@@ -0,0 +1,12 @@
+package tn.esprit.Services;
+
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+@Slf4j
+public class Services implements IServices {
+}
